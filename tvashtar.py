9a953b7233655cccbd594bb641500bb50e18640e
Alex <harrison.alex117@gmail.com>
2019-04-07 03:37:53 -0700

    starting pygame and defining colors

---
+++ tvashtar.py
@@ -0,0 +1,13 @@
+import sys
+import math
+import random
+import pygame as pg 
+
+pg.init() # initializes pygame
+
+# defining color table
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LT_GRAY = (180, 180, 180)
+GRAY =(120, 120, 120)
+DK_GRAY = (80, 80, 80)
